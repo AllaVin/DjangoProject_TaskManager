bd99588945126e06ab3498f117f6a7f73629227f
matyashalla@gmail.com <matyashalla@gmail.com>
2025-06-11 13:30:27 +0200

    Adding Projects

--- TaskManager_app/admin.py
+++ TaskManager_app/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from TaskManager_app.models import Task, SubTask, Category
-
-
+from TaskManager_app.models import Task, SubTask, Category, Project
 
 admin.site.register(Category)
 admin.site.register(Task)
 admin.site.register(SubTask)
+admin.site.register(Project)
 
 # Register your models here.
--- TaskManager_app/models.py
+++ TaskManager_app/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 STATUS_CHOICES = [
     ('New', 'New'),
-    ('In Progress', 'In Progress'),
+    ('In_Progress', 'In_Progress'),
     ('Pending', 'Pending'),
     ('Blocked', 'Blocked'),
     ('Done', 'Done'),
 ]
+
+PRIORITY_CHOICES = [
+    ('Low', 'Low'),
+    ('Medium', 'Medium'),
+    ('High', 'High'),
+]
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -14,11 +21,21 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     title = models.CharField(max_length=100, unique=True)
-    description = models.TextField()
-    categories = models.ManyToManyField(Category, related_name='tasks')  # ✅ правильная связь
+    description = models.TextField(null=True, blank=True)
+    categories = models.ManyToManyField(Category, related_name='tasks') # M2M
     status = models.CharField(max_length=100, choices=STATUS_CHOICES, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    priority = models.CharField(max_length=15, choices=PRIORITY_CHOICES, default='Low')
     deadline = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -28,10 +45,13 @@ class Task(models.Model):
 class SubTask(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks')  # ✅ один-ко-многим
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks')  #  O2M
     status = models.CharField(max_length=100, choices=STATUS_CHOICES, null=True)
     deadline = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
+
